c6a54ba1f7a496cabc835cd0452aeccb906eace0
teaching-it <svolta.it@gmail.com>
2021-11-19 21:52:27 +0100

    4TLS: calcolo dell'area del triangolo

---
+++ 4TLS/11-nov/area_triangolo.py
@@ -0,0 +1,12 @@
+"""
+Calcolare l'area del triangolo, date le misure della base e dell'altezza.
+"""
+
+b = int(input('Misura della base: '))
+h = int(input('Misura dell\'altezza :'))
+
+# Calcolo dell'area
+a = (b * h) / 2
+
+# Output a video del risultato
+print('La misura dell\'area è: ', a)
