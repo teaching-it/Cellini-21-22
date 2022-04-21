edaa35b2c3b3d37d89f74d897f429a71a28f8869
teaching-it <svolta.it@gmail.com>
2022-04-21 21:49:26 +0200

    4TLS: OOP Garage indentation revision

--- 4TLS/07_08-apr-22/Garage.py
+++ 4TLS/07_08-apr-22/Garage.py
@@ -2,40 +2,43 @@
 # Classe Auto #
 ###############
 class Auto():
-	# Metodo costruttore: avviato in fase di creazione di un'oggetto,
+    # Metodo costruttore: avviato in fase di creazione di un'oggetto,
     # ovvero di un'istanza della classe Auto, ovvero di un "esemplare" della classe Auto
-	def __init__(self, targa, modello, ora_entrata):
-		self.targa = targa
-		self.modello = modello
-		self.ora_entrata = ora_entrata
-	
-	def get_targa(self):
-		return self.targa
-		
-	def get_modello(self):
-		return self.modello
-	
-	def get_ora_entrata(self):
-		return self.ora_entrata
-		
+    def __init__(self, targa, modello, ora_entrata):
+        self.targa = targa
+        self.modello = modello
+        self.ora_entrata = ora_entrata
+
+    def get_targa(self):
+        return self.targa
+
+    def get_modello(self):
+        return self.modello
+
+    def get_ora_entrata(self):
+        return self.ora_entrata
+
 #################
 # Classe Garage #
 #################
+
+
 class Garage():
-	def __init__(self, capienza):
-		self.capienza = capienza
-		# Creo una lista di auto vuota (di oggetti di tipo Auto)
-		self.auto_list = []
-	
-	def add_auto(self, auto):
-		# Se i posti disponibili sono sufficienti: aggiungo un auto alla lista delle auto
-		if len(self.auto_list) <= self.capienza:
-			self.auto_list.append(auto)
-			
-	def get_last_auto(self):
-		# Restituisco l'ultima auto entrata nel garage
-		return self.auto_list[-1]
-		
+   	def __init__(self, capienza):
+        self.capienza = capienza
+        # Creo una lista di auto vuota (di oggetti di tipo Auto)
+        self.auto_list = []
+
+    def add_auto(self, auto):
+        # Se i posti disponibili sono sufficienti: aggiungo un auto alla lista delle auto
+        if len(self.auto_list) <= self.capienza:
+            self.auto_list.append(auto)
+
+    def get_last_auto(self):
+        # Restituisco l'ultima auto entrata nel garage
+        return self.auto_list[-1]
+
+
 ########
 # Main #
 ########
